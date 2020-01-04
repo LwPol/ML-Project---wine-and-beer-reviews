dedc072ed31c02af89493bfe2a3053e51a9a2da0
mzy <mzy@hicron.com>
2020-01-04 22:06:30 +0100

    language filtering finished

--- dataset_filtering.py
+++ dataset_filtering.py
@@ -2,10 +2,12 @@ import csv
 import glob
 
 from langdetect import detect
+from nltk.corpus import wordnet as wn
 
-line_count = 0
-dataset_csv = 'beer_dataset.csv'
-english_reviews_only_file = 'english_reviews.csv'
+
+# line_count = 0
+# dataset_csv = 'beer_dataset.csv'
+# english_reviews_only_file = 'english_reviews.csv'
 
 
 def save_to_one_file():
@@ -64,9 +66,52 @@ def proceed(csv_file):
 
 
 def count_number_of_lines():
-    for file_name in glob.glob('./beer_reviews_folder/beer_reviews*.csv'):
-        with open(file_name, encoding='utf-8', errors='ignore') as csv_file:
+    for file_name in glob.glob('english_reviews.csv'):
+        with open(file_name, encoding='utf-8') as csv_file:
             proceed(csv_file)
 
 
-filter_by_language(dataset_csv, 'en', english_reviews_only_file)
+# filter_by_language(dataset_csv, 'en', english_reviews_only_file)
+def filter_by_verb_type(input_file_name):
+    valid_rec_counter = 0
+    with open(input_file_name, mode='r', encoding='utf-8', errors='ignore') as input_file:
+        csv_reader = csv.reader(input_file, delimiter=';')
+        for row in csv_reader:
+            if len(row) < 4:
+                print(row)
+            else:
+                words = str.split(row[4])
+                adj_counter = 0
+                for w in words:
+                    for synset in wn.synsets(w):
+                        if synset.pos() == 'a':
+                            adj_counter += 1
+                            break
+                if adj_counter >= 10:
+                    valid_rec_counter += 1
+    print("Number of reviews with more than 15 adjectives:" + str(valid_rec_counter))
+
+
+# filter_by_verb_type("english_reviews.csv")
+
+
+def count_adj(sentence):
+    words = str.split(sentence)
+    adj_counter = 0
+    for w in words:
+        for synset in wn.synsets(w):
+            if synset.pos() == 'a':
+                adj_counter += 1
+                break
+    return adj_counter
+
+
+def count(sentence, type):
+    words = str.split(sentence)
+    verb_counter = 0
+    for w in words:
+        for synset in wn.synsets(w):
+            if synset.pos() == type:
+                verb_counter += 1
+                break
+    return verb_counter
